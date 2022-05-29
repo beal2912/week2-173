c53164a559a8e2bad7e702d1d247a6db8a1cc707
beal2912 <beal2912@hotmail.com>
2022-05-29 17:41:14 +0200

    play moves

--- x/checkers/types/codec.go
+++ x/checkers/types/codec.go
@@ -3,16 +3,26 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
-	// this line is used by starport scaffolding # 1
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	// this line is used by starport scaffolding # 2
+	cdc.RegisterConcrete(&MsgPlayMove{}, "checkers/PlayMove", nil)
+
+	cdc.RegisterConcrete(&MsgCreateGame{}, "checkers/CreateGame", nil)
+
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	// this line is used by starport scaffolding # 3
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgPlayMove{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCreateGame{},
+	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
